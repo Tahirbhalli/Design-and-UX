3eb3af73279c0d9cee8b7ae230ea0b8a64a08176
Tahir Ahmad <tahirbhalli16@gmail.com>
2020-02-06 22:25:43 +0500

    Update stylelint.config.js

--- stylelint.config.js
+++ stylelint.config.js
@@ -105,7 +105,6 @@ module.exports = {
         "value-list-comma-newline-after": "always-multi-line",
         "value-list-comma-space-after": "always-single-line",
         "value-list-comma-space-before": "never",
-        "--ignore=box-model",
         "value-list-max-empty-lines": 0,
     },
 }
